7271eee31e210b32f92e81b35911d3343a0b79fe
Alief Arifin M <aliefarifin99@gmail.com>
2025-01-08 18:20:47 +0700

    add android token verified

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -1,5 +1,7 @@
 const passport = require("passport");
+const axios = require("axios");
 const jwt = require("jsonwebtoken");
+const { findOrCreateUser } = require("../models/usersModel");
 const { createDefaultWallets } = require("../models/walletModel");
 const { createDefaultCategory } = require("../models/categoryModel");
 
@@ -31,4 +33,49 @@ const googleCallback = (req, res, next) => {
   )(req, res, next);
 };
 
-module.exports = { googleLogin, googleCallback };
+const googleLoginMobile = async (req, res) => {
+  const { idToken } = req.body;
+
+  try {
+    // Verifikasi token dengan Google API
+    const response = await axios.get(
+      `https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`
+    );
+    const payload = response.data;
+
+    // Validasi audience dari token
+    if (payload.aud !== process.env.GOOGLE_CLIENT_ID) {
+      return res.status(401).json({ message: "Invalid token audience" });
+    }
+
+    // Buat atau cari user berdasarkan payload
+    const user = await findOrCreateUser({
+      id: payload.sub,
+      displayName: payload.name,
+      emails: [{ value: payload.email }],
+    });
+
+    // Generate JWT untuk user
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    if (user.isNewUser) {
+      await createDefaultWallets(user.id);
+      await createDefaultCategory(user.id);
+    }
+
+    res.json({
+      message: "Login successful",
+      user: user.username,
+      token,
+    });
+  } catch (error) {
+    console.error("Error verifying Google token:", error.message);
+    res.status(401).json({ message: "Invalid Google token" });
+  }
+};
+
+module.exports = { googleLogin, googleCallback, googleLoginMobile };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const {
 const { register, login } = require("../controllers/usersController");
 
 router.get("/google", googleLogin);
+router.get("/google/mobile", googleLogin);
 router.get("/google/callback", googleCallback);
 router.post("/register", register);
 router.post("/login", login);
