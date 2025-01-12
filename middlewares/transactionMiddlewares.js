4a95e6df4e1a800c7706513b2eefdf63c0bf85b1
Alief Arifin M <aliefarifin99@gmail.com>
2025-01-12 10:51:13 +0700

    add transaction middleware, and update transaction endpoint

--- middlewares/transactionMiddlewares.js
+++ middlewares/transactionMiddlewares.js
@@ -1,24 +1,26 @@
 const db = require("../config/db");
 
 const verifyTransactionOwnership = async (req, res, next) => {
-    const { transaction_id } = req.params;
-    const user_id = req.user.id; 
-  
-    try {
-      const [transaction] = await db.query(
-        "SELECT * FROM transactions WHERE id = ? AND user_id = ?",
-        [transaction_id, user_id]
-      );
-  
-      if (!transaction) {
-        return res.status(404).json({ error: "Transaction not found or not authorized to access" });
-      }
-        
-      next();
-    } catch (error) {
-      console.error("Error checking transaction ownership:", error);
-      return res.status(500).json({ error: "Internal server error" });
+  const { transaction_id } = req.params;
+  const user_id = req.user.id;
+
+  try {
+    const [transaction] = await db.query(
+      "SELECT * FROM transactions WHERE id = ? AND user_id = ?",
+      [transaction_id, user_id]
+    );
+
+    if (!transaction) {
+      return res
+        .status(404)
+        .json({ error: "Transaction not found or not authorized to access" });
     }
-  };
+
+    next();
+  } catch (error) {
+    console.error("Error checking transaction ownership:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 module.exports = verifyTransactionOwnership;
